431cecbfe1a3dccda1e145be494ce99547376915
ara-bara <archomoris2510@gmail.com>
2026-02-24 21:55:41 +0200

    fix: use vite base url for router basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/redwing-studio-pracrice">
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <StrictMode>
       <App />
     </StrictMode>
